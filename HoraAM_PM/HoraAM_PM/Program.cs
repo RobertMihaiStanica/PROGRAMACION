4e84348d997b14e0d5a7545090a65bd9a76bf7ab
RobertMihaiStanica <robert.mihai@educa.madrid.org>
2022-09-29 12:30:51 +0200

    Added loop to the program
    Added a loop that will ask for the number again if the user has given a wrong input

--- HoraAM_PM/HoraAM_PM/Program.cs
+++ HoraAM_PM/HoraAM_PM/Program.cs
@@ -1,35 +1,46 @@
-﻿//Here we declare our variables
-
-int hour;
-
-//This line asks the user for the hour they want to convert
-
-Console.WriteLine("Introduce la hora en formato 24h: ");
-
-//This try will catch an exception if the user doesn´t input a number
-
-try
+﻿internal class Program
 {
-    hour = Convert.ToInt32(Console.ReadLine());
-}
-catch (FormatException)
-{
-    Console.WriteLine("Error: no se ha introducido un caracter valido");
-    return;
-}
+    private static void Main(string[] args)
+    {
+        //We are declaring the variable and giving it a blank value for the code to work properly
 
-//Here we do the conversion
+        int hour = -1;
 
-if (hour > 24 ^ hour < 0)
-{
-    Console.WriteLine("El numero intorducido no corresponde a un hora valida");
-}
-else if (hour > 12)
-{
-    hour = hour - 12;
-    Console.WriteLine("Son las " + hour + "PM");
-}
-else
-{
-    Console.WriteLine("Son las " + hour + "AM");
+        //This line asks the user for the hour they want to convert
+
+        Console.WriteLine("Introduce la hora en formato 24h: ");
+
+        /*This try will catch an exception if the user doesn´t input a number
+         The "do" function will create a loop that asks the user to type the number again*/
+
+        do
+        {
+            try
+            {
+                hour = Convert.ToInt32(Console.ReadLine());
+
+                if (hour < 0 ^ hour > 24)
+                {
+                    Console.WriteLine("El valor introducido no corresponde a una hora, intentalo de nuevo: ");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: no se ha introducido un caracter valido, intentalo de nuevo: ");
+            }
+        } while (hour <= -1 ^ hour > 24);
+
+        //Here we do the conversion
+
+        
+        if (hour > 12)
+        {
+            hour = hour - 12;
+            Console.WriteLine("Son las " + hour + "PM");
+        }
+        else
+        {
+            Console.WriteLine("Son las " + hour + "AM");
+        }
+    }
 }
