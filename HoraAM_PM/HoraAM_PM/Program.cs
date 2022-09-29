bfa5aafb5de08ca7d497c9cdadd8f72aa4732fed
RobertMihaiStanica <robert.mihai@educa.madrid.org>
2022-09-29 10:33:09 +0200

    Nuevas caracteristicas del conversor de horas
    Se añade un mensaje de error cuando se introduce algo que no es un numero
    Se establece un mensaje de error en caso de que el numero no sea valido
    Se agregan comentarios para explicar cada parte del programa

--- HoraAM_PM/HoraAM_PM/Program.cs
+++ HoraAM_PM/HoraAM_PM/Program.cs
@@ -1,8 +1,30 @@
-﻿Console.WriteLine("Introduce la hora en formato 24h: ");
+﻿//Here we declare our variables
 
-int hour = Convert.ToInt32(Console.ReadLine());
+int hour;
 
-if (hour > 12)
+//This line asks the user for the hour they want to convert
+
+Console.WriteLine("Introduce la hora en formato 24h: ");
+
+//This try will catch an exception if the user doesn´t input a number
+
+try
+{
+    hour = Convert.ToInt32(Console.ReadLine());
+}
+catch (FormatException)
+{
+    Console.WriteLine("Error: no se ha introducido un caracter valido");
+    return;
+}
+
+//Here we do the conversion
+
+if (hour > 24 ^ hour < 0)
+{
+    Console.WriteLine("El numero intorducido no corresponde a un hora valida");
+}
+else if (hour > 12)
 {
     hour = hour - 12;
     Console.WriteLine("Son las " + hour + "PM");
